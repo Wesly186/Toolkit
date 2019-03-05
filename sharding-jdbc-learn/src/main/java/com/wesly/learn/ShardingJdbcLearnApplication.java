e9b914815d687479082268c4fbef8349b6fe177a
xiang.wei <652048614@qq.com>
2019-03-05 11:42:30 +0800

    start sharding-jdbc

---
+++ sharding-jdbc-learn/src/main/java/com/wesly/learn/ShardingJdbcLearnApplication.java
@@ -0,0 +1,13 @@
+package com.wesly.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcLearnApplication.class, args);
+    }
+
+}
